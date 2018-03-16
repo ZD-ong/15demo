3541099c8e059f67a361345bdb36e15322008659
ZD-ong <384474574@qq.com>
2018-03-16 19:16:00 +0800

    实现自动轮播

--- carousel/script.js
+++ carousel/script.js
@@ -1,3 +1,11 @@
+// 1. 实现点击轮播
+// 2. 实现自动轮播
+// 3. 实现高亮点击按钮
+// 4. 鼠标浮在图片上，图片停止自动轮播
+// 5. 鼠标离开图片，图片继续轮播
+// 6. 当鼠标浮在按钮上，图片停止自动轮播
+
+
 var allButtons = $('#buttons > span') //allButtons是一个伪数组
 for(let i=0;i<allButtons.length;i++){
     $(allButtons[i]).on('click',function(x){
@@ -7,12 +15,35 @@ for(let i=0;i<allButtons.length;i++){
         $('#images').css({
             transform: 'translate(' + p + 'px)'
         })
+        n = index //重置n
+        allButtons.eq(n)
+            .addClass('red')
+            .siblings('.red').removeClass('red')
     })
 }
 
-// var n =0
-// console.log(n%4)//四个一循环
-// setInterval(()=>{
-//     n += 1
-//     console.log(n%4)
-// },1000)
+var n =0
+var size = allButtons.length
+allButtons.eq(n%size).trigger('click')
+    .addClass('red')
+    .siblings('.red').removeClass('red')
+
+var timer = setInterval(()=>{
+    n += 1
+    allButtons.eq(n%size).trigger('click')
+        .addClass('red')
+        .siblings('.red').removeClass('red')
+},3000)
+
+$('.window').on('mouseenter',function(){
+    window.clearInterval(timer)
+})
+
+$('.window').on('mouseleave',function(){
+    timer = setInterval(() => {
+        n += 1
+        allButtons.eq(n % size).trigger('click')
+            .addClass('red')
+            .siblings('.red').removeClass('red')
+    }, 1000)
+})
